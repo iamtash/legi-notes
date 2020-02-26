79535d52ea8a9159d4fe2ff63b3a8cc34c83a384
iamtash <natashachernis@gmail.com>
2020-02-25 23:23:00 -0600

    render list of bills in stackable cards display

--- client/src/components/BillList.js
+++ client/src/components/BillList.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import BillItem from './BillItem'
 
-const BillList = ({bill}) => {
-    return <div>BillList</div>
+const BillList = ({ bills, onBillSelect }) => {
+    const renderedList = bills.map(bill => {
+        return (
+            <BillItem
+                key={bill.bill_id} 
+                bill={bill}
+                onBillSelect={onBillSelect}
+            />
+        )
+    })
+    return <div className="ui three stackable cards">{renderedList}</div>
 }
 
 export default BillList
