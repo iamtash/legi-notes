966dbe9d70154a2f173d0017bf9ef11cf92063bb
iamtash <natashachernis@gmail.com>
2020-02-29 01:42:18 -0600

    set up thunk getBills action creator that fetches the bills and dispatches the data to the reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,22 @@
+import propublica from '../apis/propublica.js'
+
 export const signIn = () => {
     return { type: 'SIGN_IN' }
 }
 
 export const signOut = () => {
     return { type: 'SIGN_OUT' }
+}
+
+export const getBills = term => async dispatch => {
+    const response = await propublica.get('/search.json', {
+        params: {
+            query: term
+        }
+    })
+ 
+    dispatch({ 
+        type: 'GET_BILLS',
+        payload: response.data.results[0].bills
+    })
 }
