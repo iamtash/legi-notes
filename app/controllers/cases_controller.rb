504fc67765cef1efeecbfb300c010cc66a3ecf95
iamtash <natashachernis@gmail.com>
2020-03-01 15:17:46 -0600

    send user's cases response

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,5 +1,9 @@
 class CasesController < ApplicationController
   def index
+    user = User.find_by(id: params[:user_id])
+    render json: user.cases.to_json(include: {
+      bookmarks: { only: [:url, :bill_number]}
+    })
   end
 
   def create
