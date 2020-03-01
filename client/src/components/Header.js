38e33d38f8f56b29ecd87b917d7c2cca6ced4fb3
iamtash <natashachernis@gmail.com>
2020-03-01 11:00:52 -0600

    conditionally render Bookmarks link depending on signin status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Avatar from './Avatar'
 import GoogleAuth from './GoogleAuth'
 
-const Header = () => {
+const Header = ({ isSignedIn }) => {
+    const renderLinks = () => {
+        return (isSignedIn ? <Link to="/bookmarks" className="item">Bookmarks</Link> : null)
+    }
+
     return (
         <div className="ui secondary pointing menu">
-            <Link to="/" className="item">LegiNotes</Link>
+            <Link to="/" className="item header">LegiNotes</Link>
+            {renderLinks()}
             <div className="right menu">
+                <Avatar style={{marginRight: '1em'}} />
                 <GoogleAuth />
             </div>
         </div>
     )
 }
 
-export default Header
+const mapStateToProps = state => {
+    return { isSignedIn: state.auth.isSignedIn }
+}
+
+export default connect(mapStateToProps)(Header)
