946302c21805b71a3dfaadd997197deed69048d3
iamtash <natashachernis@gmail.com>
2020-03-01 10:48:26 -0600

    write create bookmarks, whitelist params

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,23 @@
 class BookmarksController < ApplicationController
   def create
+    bookmark = Bookmark.new(bookmark_params)
+    if bookmark.save
+      render json: bookmark.to_json
+    else
+      render json: { status: 500 }
+    end
   end
 
   def destroy
   end
+
+  private
+    def bookmark_params
+      params.require(:bookmark).permit(
+        :bill_number,
+        :url,
+        :case_id,
+        :case_attributes
+      )
+    end
 end
