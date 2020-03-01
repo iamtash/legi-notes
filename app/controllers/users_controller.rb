3c8aecc6a2c6131bb9f4ac5cd051032aa1c29323
iamtash <natashachernis@gmail.com>
2020-03-01 10:50:19 -0600

    ifentify or create user if google id token is verified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def create
+    authenticator = Authenticator.new(ENV['GOOGLE_CLIENT_ID'])
+    if authenticator.valid_credentials?(token)
+      user = User.find_or_initialize_by(email: authenticator.get_payload['email'])
+      if user.save 
+        render json: { id: user.id, email: user.email }
+      else 
+        render json: { status: 500 }
+      end
+    else
+      render json: { status: 403 }
+    end
+    
   end
+  
+
+  private
+    def token
+      params.require(:idtoken)
+    end
 end
