e676a293dc35e64a424fa8f1cb5ff446fdff0a06
iamtash <natashachernis@gmail.com>
2020-03-01 10:56:41 -0600

    store selectedBill in redux store, add Routes to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,13 +5,13 @@ import Header from './Header'
 import SearchBar from './SearchBar'
 import BillDetail from './bills/BillDetail'
 import BillList from './bills/BillList'
+import CaseList from './cases/CaseList'
+import NewBookmark from './containers/NewBookmark'
 import { getBills } from '../actions'
+import { selectBill } from '../actions'
 
 class App extends React.Component {
-    state = { 
-        selectedBill: {},
-        modalOpen: false
-    }
+    state = { modalOpen: false }
 
     componentDidMount() {
         this.onTermSubmit('tax')
@@ -21,9 +21,9 @@ class App extends React.Component {
         this.props.getBills(term)
     }
 
-
     onBillSelect = bill => {
-        this.setState({ selectedBill: bill, modalOpen: true })
+        this.props.selectBill(bill)
+        this.setState({ modalOpen: true })
     }
 
     onModalClose = () => {
@@ -35,13 +35,21 @@ class App extends React.Component {
             <div className="ui container"> 
                 <BrowserRouter>
                     <Header />
-                    <SearchBar onFormSubmit={this.onTermSubmit} />
-                    <BillDetail bill={this.state.selectedBill} open={this.state.modalOpen} onModalClose={this.onModalClose} />
-                    <BillList onBillSelect={this.onBillSelect} />
+                    <Route path="/" exact>
+                        <SearchBar onFormSubmit={this.onTermSubmit} />
+                        <BillDetail bill={this.props.selectedBill} open={this.state.modalOpen} onModalClose={this.onModalClose} />
+                        <BillList onBillSelect={this.onBillSelect} />
+                    </Route>
+                    <Route path="/bookmarks" exact component={CaseList} />
+                    <Route path="/bookmarks/new" exact component={NewBookmark} />
                 </BrowserRouter>
             </div>
         )
     }
 }
 
-export default connect(null, { getBills })(App)
+const mapStateToProps = state => {
+    return { selectedBill: state.selectedBill }
+}
+
+export default connect(mapStateToProps, { getBills, selectBill })(App)
