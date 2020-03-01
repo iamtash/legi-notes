40fff64b29408ad3ac6667defdbc5fbd4e210620
iamtash <natashachernis@gmail.com>
2020-03-01 11:20:09 -0600

    add new reducers to combineReducers()

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from 'redux'
 import authReducer from './authReducer.js'
 import billsReducer from './billsReducer.js'
+import currentUserReducer from './currentUserReducer'
+import loadingReducer from './loadingReducer'
+import casesReducer from './casesReducer'
+import selectedBillReducer from './selectedBillReducer'
 
 export default combineReducers({
     auth: authReducer,
-    bills: billsReducer
+    bills: billsReducer,
+    currentUser: currentUserReducer,
+    loading: loadingReducer,
+    cases: casesReducer,
+    selectedBill: selectedBillReducer
 })
