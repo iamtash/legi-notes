04498d7068cf03fca38fcd61bd6d0d75093f73a1
iamtash <natashachernis@gmail.com>
2020-03-01 15:23:28 -0600

    re-style select option msg component

--- client/src/components/cases/SelectOption.js
+++ client/src/components/cases/SelectOption.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Segment, Button } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 
 const SelectOption = props => {
     return (
-        <Segment raised>
-            Select a case to add your bookmark to or 
-            <Button 
-                basic 
-                color="blue"
-                style={{marginLeft: '5px'}}
-                onClick={props.onShowFormClick}
-            >Create a new case
-            </Button>
-        </Segment>
+        <div className="ui compact blue message">
+            <div className="header">
+                Select a case to add your bookmark to or 
+                <Button 
+                    inverted
+                    secondary
+                    style={{marginLeft: '1em'}}
+                    onClick={props.onShowFormClick}
+                >Create a new case
+                </Button>
+            </div>
+        </div>
     )
 }
 
