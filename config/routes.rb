82a840d1cd3861e8ef5b8beb895f481bf6253ff9
iamtash <natashachernis@gmail.com>
2020-03-01 11:21:22 -0600

    route requests from frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
-  get 'cases/index'
-  get 'cases/create'
-  get 'cases/update'
-  get 'cases/destroy'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'users' => 'users#create'
+  get 'cases' => 'cases#index'
+  post 'bookmarks' => 'bookmarks#create'
 end
