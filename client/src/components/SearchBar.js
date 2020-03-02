eb6c61c8d2c6919edbb9cc6e48ef09cb4147b32f
iamtash <natashachernis@gmail.com>
2020-03-01 18:17:21 -0600

    display msg about search results

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Input } from 'semantic-ui-react'
+import { Input, Message } from 'semantic-ui-react'
 
 class SearchBar extends React.Component {
-    state = { term: '' }
+    state = { term: '', message: '' }
 
     onInputChange = event => {
-        this.setState({ term: event.target.value })
+        this.setState({ term: event.target.value, message: false })
     }
 
     onFormSubmit = event => {
         event.preventDefault()
         this.props.onFormSubmit(this.state.term)
+        this.setState(prevState => ({ term: '', message: prevState.term }))
     }
 
     renderInput = () => {
@@ -35,6 +36,12 @@ class SearchBar extends React.Component {
         }
     }
 
+    renderMessage = () => {
+        if (!this.props.loading && this.state.message) {
+            return <Message positive compact size="mini" >Search results for <b>{this.state.message}</b></Message>
+        }
+    }
+
     render() {
         return (
             <div className="search-bar ui segment raised">
@@ -44,6 +51,7 @@ class SearchBar extends React.Component {
                         {this.renderInput()}
                     </div>
                 </form>
+                {this.renderMessage()}
             </div>
         )
     }
