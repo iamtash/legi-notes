ba2a299cea00e4e3165187cde4397f8a9e7a65b3
iamtash <natashachernis@gmail.com>
2020-03-01 22:51:35 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 user = User.first
-user.cases.create(number: '1A', title: 'Important', client: 'Best Buy')
-user.cases.create(number: '2B', title: 'Expensive', client: 'Trump')
-user.cases.create(number: '3C', title: 'Tricky', client: 'Elon Musk')
+user.cases.create(number: '1234', title: 'UT v. TAMU', client: 'UT Legal')
+user.cases.create(number: '5678', title: 'Harris County Pollution Fine', client: 'Shell Legal')
+user.cases.create(number: '4567', title: 'Dell Intellectual Property Theft', client: 'Dell Legal')
