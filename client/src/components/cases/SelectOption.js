0a4b88ec449e53e380a13e3003f1fce0303f3be7
iamtash <natashachernis@gmail.com>
2020-03-09 14:51:45 -0500

    change msg accordingly if no cases have been created yet

--- client/src/components/cases/SelectOption.js
+++ client/src/components/cases/SelectOption.js
@@ -1,21 +1,50 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import './SelectOption.css'
 
 const SelectOption = props => {
+    const renderMsg = () => {
+        if (props.cases.length < 1) {
+            return (
+                <>
+                {renderButton()}
+                <div className="inline-header header" style={{marginLeft: '1em'}}>
+                    to add your bookmark to.
+                </div>
+                </>
+            )
+        } else {
+            return (
+                <>
+                <div className="header inline-header">
+                    Select a case to add your bookmark to or 
+                </div>
+                {renderButton()}
+                </>
+            )
+        }
+    }
+
+    const renderButton = () => (
+        <Button 
+            inverted
+            secondary
+            style={{marginLeft: '1em'}}
+            onClick={props.onShowFormClick}
+        >Create a new case
+        </Button>
+    )
+
     return (
         <div className="ui compact blue message">
-            <div className="header">
-                Select a case to add your bookmark to or 
-                <Button 
-                    inverted
-                    secondary
-                    style={{marginLeft: '1em'}}
-                    onClick={props.onShowFormClick}
-                >Create a new case
-                </Button>
-            </div>
+            {renderMsg()}
         </div>
     )
 }
 
-export default SelectOption
+const mapStateToProps = state => {
+    return { cases: state.cases }
+}
+
+export default connect(mapStateToProps)(SelectOption)
